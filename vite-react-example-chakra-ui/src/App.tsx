e4142d3fb0934fc8a940eab6cdba83f922b524f3
MasuqaT <occar@hotmail.co.jp>
2021-11-27 20:21:06 +0900

    Replace with chakra ui

--- vite-react-example-chakra-ui/src/App.tsx
+++ vite-react-example-chakra-ui/src/App.tsx
@@ -1,45 +1,99 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import {
+  Box,
+  Button,
+  Code,
+  Flex,
+  Image,
+  keyframes,
+  Link,
+  Text,
+  usePrefersReducedMotion,
+} from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const prefersReducedMotion = usePrefersReducedMotion();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+    <Box textAlign="center">
+      <Flex
+        as="header"
+        bgColor="theme.navy"
+        minHeight="100vh"
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        fontSize="calc(10px + 2vmin)"
+        color="white"
+        sx={{
+          gap: 48,
+        }}
+      >
+        <Image
+          src={logo}
+          alt="logo"
+          height="40vmin"
+          pointerEvents="none"
+          animation={
+            prefersReducedMotion ? undefined : `${logoSpin} infinite 20s linear`
+          }
+        />
+        <Text>Hello Vite + React!</Text>
+        <Text>
+          <Button
+            colorScheme="gray"
+            bgColor="gray.200"
+            variant="outline"
+            color="black"
+            padding="2"
+            fontSize="inherit"
+            fontWeight="inherit"
+            onClick={() => setCount((count) => count + 1)}
+          >
             count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
+          </Button>
+        </Text>
+        <Text>
+          Edit{" "}
+          <Code colorScheme="transparent" fontSize="inherit">
+            App.tsx
+          </Code>{" "}
+          and save to test HMR updates.
+        </Text>
+        <Text>
+          <Link
             href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+            isExternal
+            color="theme.cyan"
+            textDecoration="underline"
           >
             Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
+          </Link>
+          {" | "}
+          <Link
             href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
+            isExternal
+            color="theme.cyan"
+            textDecoration="underline"
           >
             Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+          </Link>
+        </Text>
+      </Flex>
+    </Box>
+  );
 }
 
-export default App
+export default App;
+
+const logoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
--- vite-react-example-chakra-ui/src/main.tsx
+++ vite-react-example-chakra-ui/src/main.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    body: "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue'",
+    mono: "source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace",
+  },
+  colors: {
+    theme: { navy: "#282c34", cyan: "#61dafb" },
+  },
+  styles: {
+    global: {
+      body: {
+        "-webkit-font-smoothing": "antialiased",
+        "-moz-osx-font-smoothing": "grayscale",
+        fontFeatureSettings: "unset",
+      },
+      code: {
+        fontFamily: "mono",
+      },
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
