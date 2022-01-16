7cf0b685460f21faf050ad835f902e894dc5683d
MasuqaT <occar@hotmail.co.jp>
2022-01-16 19:35:53 +0900

    fix text align

--- decoration-css-decomposition/src/App.tsx
+++ decoration-css-decomposition/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div style={{ textAlign: "center" }}>
+    <div className="App">
       <header
         style={{
           minHeight: "100vh",
