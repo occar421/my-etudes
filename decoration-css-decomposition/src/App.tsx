44c54e2f966de7480387b847171ea3d27e4ccc60
MasuqaT <occar@hotmail.co.jp>
2022-01-16 22:01:06 +0900

    use grouped style

--- decoration-css-decomposition/src/App.tsx
+++ decoration-css-decomposition/src/App.tsx
@@ -1,23 +1,26 @@
 import { useState } from "react";
 import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div className={baseStyle}>
       <header
         w--min-h="screen"
         w--flex="~ col" // gap-y-48px after https://github.com/windicss/windicss/issues/582
         w--align="items-center"
         w--justify="content-center"
-        className="gap-y-48px App-header"
+        className={`gap-y-48px ${headerStyle}`}
       >
-        <img src={logo} w--h="[40vmin]" className="App-logo" alt="logo" />
+        <img src={logo} w--h="[40vmin]" className={logoStyle} alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button
+            type="button"
+            className={buttonStyle}
+            onClick={() => setCount((count) => count + 1)}
+          >
             count is: {count}
           </button>
         </p>
@@ -26,7 +29,7 @@ function App() {
         </p>
         <p>
           <a
-            className="App-link"
+            className={linkStyle}
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
@@ -35,7 +38,7 @@ function App() {
           </a>
           {" | "}
           <a
-            className="App-link"
+            className={linkStyle}
             href="https://vitejs.dev/guide/features.html"
             target="_blank"
             rel="noopener noreferrer"
@@ -49,3 +52,15 @@ function App() {
 }
 
 export default App;
+
+const baseStyle = "text-center";
+
+const headerStyle = "bg-theme-navy text-24px text-white";
+
+const logoStyle =
+  "pointer-events-none animate-spin animate-duration-20s motion-reduce:transition-none";
+
+const linkStyle = "text-theme-cyan underline";
+
+const buttonStyle =
+  "bg-gray-200 text-black border-gray-500 border-2 px-2 text-24px hover:bg-gray-300";
