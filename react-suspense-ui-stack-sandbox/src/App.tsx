a63b0ac0d88ffa0bd980688676da895312fa003e
MasuqaT <occar@hotmail.co.jp>
2022-05-29 22:54:42 +0900

    2 kinds of loading

--- react-suspense-ui-stack-sandbox/src/App.tsx
+++ react-suspense-ui-stack-sandbox/src/App.tsx
@@ -4,13 +4,16 @@ import {
   Suspense,
   useContext,
   useState,
+  useTransition,
 } from "react";
 
 const DataGetterContext = createContext<(key: string) => string | undefined>(
   () => undefined
 );
 
-const DataSetterContext = createContext((_: string, __: string) => {});
+const DataSetterContext = createContext(
+  (_: string, __: string | undefined) => {}
+);
 
 const DataClearContext = createContext(() => {});
 
@@ -20,7 +23,13 @@ const CacheProvider = ({ children }: { children: ReactNode }) => {
   return (
     <DataGetterContext.Provider value={(key) => dataMap.get(key)}>
       <DataSetterContext.Provider
-        value={(key, value) => dataMap.set(key, value)}
+        value={(key, value) => {
+          if (value === undefined) {
+            dataMap.delete(key);
+          } else {
+            dataMap.set(key, value);
+          }
+        }}
       >
         <DataClearContext.Provider value={() => setDataMap(new Map())}>
           {children}
@@ -32,49 +41,108 @@ const CacheProvider = ({ children }: { children: ReactNode }) => {
 
 export const App = () => {
   const [delay, setDelay] = useState(1000);
+  const [generation, setGeneration] = useState(0);
+
+  const cacheKey = "data-" + generation;
+
+  const [nextLoading, startNextLoadingTransition] = useTransition();
 
   return (
     <CacheProvider>
-      <div className="p-6">
-        <div className="flex gap-1">
-          <label>
-            <span>Delay:</span>
-            <input
-              className="ml-2 border"
-              type="number"
-              value={delay}
-              min={0}
-              max={5000}
-              step={500}
-              onChange={(e) => setDelay(e.target.valueAsNumber)}
-            />
-          </label>
-          <DataClearContext.Consumer>
-            {(clear) => (
-              <button
-                type="button"
-                className="bg-gray-300"
-                onClick={() => clear()}
-              >
-                Clear cache
-              </button>
-            )}
-          </DataClearContext.Consumer>
-        </div>
-        <div>
-          <Suspense fallback={<div>Initial loading</div>}>
-            <LoadData delay={delay} />
-          </Suspense>
+      <div className="flex bg-gray-200 h-screen p-12 justify-center items-center">
+        <div className="bg-white p-2 rounded-xl w-96">
+          <nav className="flex gap-2">
+            <label>
+              <span className="after:content-[':']">Delay</span>
+              <input
+                className="ml-1 border"
+                type="number"
+                value={delay}
+                min={0}
+                max={5000}
+                step={500}
+                onChange={(e) => setDelay(e.target.valueAsNumber)}
+              />
+            </label>
+            <button
+              type="button"
+              className="bg-gray-300 px-1"
+              onClick={() => {
+                startNextLoadingTransition(() => {
+                  setGeneration((gen) => ++gen);
+                });
+              }}
+            >
+              Next load
+            </button>
+            <DataClearContext.Consumer>
+              {(clear) => (
+                <button
+                  type="button"
+                  className="bg-gray-300 px-1"
+                  onClick={() => clear()}
+                >
+                  Reset all
+                </button>
+              )}
+            </DataClearContext.Consumer>
+          </nav>
+          <main className="relative h-96 border p-2 mt-2">
+            <Suspense
+              fallback={
+                <Wall>
+                  <div className="flex flex-col justify-center items-center">
+                    <div className="mb-1">Initial loading...</div>
+                    <Spinner />
+                  </div>
+                </Wall>
+              }
+            >
+              <LoadData cacheKey={cacheKey} delay={delay} />
+            </Suspense>
+            {nextLoading ? (
+              <Wall>
+                <div className="flex flex-col justify-center items-center">
+                  <div className="mb-1">Continual loading...</div>
+                  <Spinner />
+                </div>
+              </Wall>
+            ) : null}
+          </main>
         </div>
       </div>
     </CacheProvider>
   );
 };
 
-const LoadData = ({ delay = 1000 }: { delay?: number } = {}) => {
-  const data = useData("data", { delay });
+const Wall = ({ children }: { children: ReactNode }) => (
+  <div className="absolute top-0 right-0 bottom-0 left-0 flex justify-center items-center bg-white bg-opacity-60">
+    {children}
+  </div>
+);
 
-  return <div>Data is {data}</div>;
+const Spinner = () => (
+  <div className="animate-spin h-8 w-8 border-4 border-gray-300 rounded-full border-t-transparent" />
+);
+
+const LoadData = ({
+  cacheKey,
+  delay = 1000,
+}: {
+  cacheKey: string;
+  delay?: number;
+}) => {
+  const data = useData(cacheKey, { delay });
+
+  return (
+    <ul className="border border-gray-500">
+      {[...new Array(5)].map(() => (
+        <li className="text-lg border-b border-gray-500 last:border-none p-4">
+          Data is {data}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 const sleep = (timeout: number) =>
