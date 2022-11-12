24c623cb9c894e4616ed47ecb0cf82f16cdefd89
MasuqaT <occar@hotmail.co.jp>
2022-11-12 13:33:31 +0900

    remove forgotten part

--- jotai-cache-sandbox/src/main.tsx
+++ jotai-cache-sandbox/src/main.tsx
@@ -3,21 +3,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { queryClientAtom } from "jotai/query";
 import { globalStore } from "./util";
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Provider
-        initialValues={[[queryClientAtom, queryClient]]}
-        unstable_createStore={() => globalStore}
-      >
-        <App />
-      </Provider>
-    </QueryClientProvider>
+    <Provider unstable_createStore={() => globalStore}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
