5d60d9855a739be6f8b3c92dd2390bbd9b7f286f
MasuqaT <occar@hotmail.co.jp>
2022-11-12 13:42:46 +0900

    generalize

--- jotai-cache-sandbox/src/jotai-cache-poc/index.ts
+++ jotai-cache-sandbox/src/jotai-cache-poc/index.ts
@@ -2,15 +2,57 @@ import {
   atomsWithQuery as _atomsWithQuery,
   atomsWithMutation as _atomsWithMutation,
 } from "jotai-tanstack-query";
+import type { Getter } from "jotai";
+import type {
+  MutationObserverOptions,
+  QueryObserverOptions,
+} from "@tanstack/query-core";
 
-export const atomsWithCache = <TQueryFnData>(
-  fetcher: () => Promise<TQueryFnData>
+export const atomsWithCache = <
+  TQueryFnData = unknown,
+  TError = unknown,
+  TData = TQueryFnData,
+  TQueryData = TQueryFnData
+>(
+  fetcher: QueryObserverOptions<
+    TQueryFnData,
+    TError,
+    TData,
+    TQueryData,
+    unknown[]
+  >["queryFn"],
+  getOptions?: (
+    get: Getter
+  ) => Omit<
+    QueryObserverOptions<TQueryFnData, TError, TData, TQueryData, unknown[]>,
+    "queryFn" | "queryKey"
+  >
 ) =>
-  _atomsWithQuery(() => ({
+  _atomsWithQuery((get) => ({
+    suspense: true,
+    useErrorBoundary: true,
+    ...getOptions?.(get),
     queryFn: fetcher,
   }));
 
-export const atomsWithMutation = (mutate: () => Promise<void>) =>
-  _atomsWithMutation(() => ({
+export const atomsWithMutation = <
+  TData = unknown,
+  TError = unknown,
+  TVariables = void,
+  TContext = unknown
+>(
+  mutate: MutationObserverOptions<
+    TData,
+    TError,
+    TVariables,
+    TContext
+  >["mutationFn"],
+  getOptions?: (
+    get: Getter
+  ) => MutationObserverOptions<TData, TError, TVariables, TContext>
+) =>
+  _atomsWithMutation((get) => ({
+    useErrorBoundary: true,
+    ...getOptions?.(get),
     mutationFn: mutate,
   }));
