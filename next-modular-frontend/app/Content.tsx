267fecefadfc488cb94b9329f570450925e86e7a
MasuqaT <occar@hotmail.co.jp>
2024-09-22 17:52:05 +0900

    children

--- next-modular-frontend/app/Content.tsx
+++ next-modular-frontend/app/Content.tsx
@@ -2,18 +2,18 @@
 
 import styles from "@/app/page.module.css";
 import {Counter} from "@/app/Counter";
-import {Clock} from "@/app/Clock";
+import type {Clock} from "@/app/Clock";
 // import {ModuleOf} from "@/app/_ModuleResolver/ModuleOf";
-import {Suspense, useState} from "react";
+import {ReactNode, Suspense, useState} from "react";
 
-export default function Content() {
+export default function Content({context}: { context: { Clock: ReactNode } }) {
     const [mode, setMode] = useState<"editor" | "linter">("editor");
 
     return <div>
         <p>Hello</p>
         <main className={styles.main}>
             <Counter/>
-            <Clock/>
+            {context.Clock}
         </main>
         {/*<Suspense fallback={null}>*/}
         {/*    <ModuleOf name={mode}/>*/}
--- next-modular-frontend/app/page.tsx
+++ next-modular-frontend/app/page.tsx
@@ -3,13 +3,15 @@ import "server-only";
 import styles from "./page.module.css";
 import {Suspense} from "react";
 import Content from "@/app/Content";
+import {Clock} from "@/app/Clock";
 
 export const experimental_ppr = true
 
-export default function Home() {
+export default async function Home() {
     return (
         <div className={styles.page}>
-            <Suspense fallback="loading... main content"><Content/></Suspense>
+            <Suspense fallback="loading... main content"><Content context={{Clock: <Clock/>}}>
+            </Content></Suspense>
         </div>
     );
 }
