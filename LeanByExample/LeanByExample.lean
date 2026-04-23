5385ce6f6838be183ea12fc51b7c209600333f55
MasuqaT <occar@hotmail.co.jp>
2026-04-24 00:21:09 +0900

    Add LeanByExample

---
+++ LeanByExample/LeanByExample.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `LeanByExample` library.
+-- Import modules here that should be built as part of the library.
+import LeanByExample.Basic
---
+++ LeanByExample/Main.lean
@@ -0,0 +1,4 @@
+import LeanByExample
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
