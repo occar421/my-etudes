3880237427836303198308a2f91a39547ec52fe3
MasuqaT <occar@hotmail.co.jp>
2026-08-02 21:41:30 +0900

    Use UUID v7

--- event-sourcing-sandbox/apps/server/src/module/pet/domain/Id.ts
+++ event-sourcing-sandbox/apps/server/src/module/pet/domain/Id.ts
@@ -1,10 +1,10 @@
-import { randomUUID } from "node:crypto";
 import { ValueObject } from "@/domain/ValueObject.ts";
 import { z } from "zod";
+import { v7 as uuidV7 } from "uuid";
 
-export class Id extends ValueObject(z.object({ value: z.uuidv4() /* TODO: use UUID v7 */ })) {
+export class Id extends ValueObject(z.object({ value: z.uuidv7() })) {
   public static generate(): Id {
-    return new Id({ value: randomUUID() });
+    return new Id({ value: uuidV7() });
   }
 
   public get rawValue(): string {
